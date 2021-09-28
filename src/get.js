5906535131b295ad0c4080fd77621259ec87478f
abhishek@halfsquare.in <abhishek@halfsquare.in>
2021-09-28 15:52:29 +0530

    fixing all..

--- src/get.js
+++ src/get.js
@@ -3,18 +3,16 @@ import dynamoDb from "./util/dynamodb";
 
 export const main = handler(async (event) => {
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.TABLE_NAME,
     // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'bookId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      bookId: event.pathParameters.id
-    }
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+      bookId: event.pathParameters.id, // The id of the note from the path
+    },
   };
 
   const result = await dynamoDb.get(params);
-  if ( ! result.Item) {
+  if (!result.Item) {
     throw new Error("Item not found.");
   }
 
